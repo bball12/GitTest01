984ee9484b2aec29c617bae210b8531025580258
bball11 <bball11@naver.com>
2016-05-08 23:37:27 +0900

    Adding className field on Sub01Lab001 class

--- src/org/sub01/Sub01Lab001.java
+++ src/org/sub01/Sub01Lab001.java
@@ -1,7 +1,15 @@
 package org.sub01;
 
 public class Sub01Lab001 {
+	private String className = "";
 	public Sub01Lab001() {
 		
 	}
+	public String getClassName() {
+		return className;
+	}
+	public void setClassName(String className) {
+		this.className = className;
+	}
+	
 }
