cfcdc8c2fd5bd77a8b5d66a886b2905923f120c3
bball11 <bball11@naver.com>
2016-05-09 19:54:06 +0900

    Organizing source

--- src/org/kwk/dev/util/EntryPoint.java
+++ src/org/kwk/dev/util/EntryPoint.java
@@ -2,6 +2,6 @@ package org.kwk.dev.util;
 
 public class EntryPoint {
 	public static void main(String[] args) {
-		System.out.println("....");
+		System.out.println("org.kwk.dev.util.EntryPoint class entrypoint");
 	}
 }
